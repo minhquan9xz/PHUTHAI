454d2cd0cea03a0b87024308f4969586fa4eaae7
minhquan9xz <minhquan9xz@gmail.com>
2021-10-14 15:22:36 +0700

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { NavComponent } from './components/header/nav/nav.component';
+import { BannerComponent } from './banner/banner.component';
+import { ContentComponent } from './content/content.component';
+import { NewsComponent } from './news/news.component';
+import { LogoComponent } from './Footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopbarComponent,
+    NavComponent,
+    BannerComponent,
+    ContentComponent,
+    NewsComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
